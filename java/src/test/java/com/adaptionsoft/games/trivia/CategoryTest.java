93d3d0641068a7ba37a99c501c22afeac3788231
Maarten Gregoire <maarten.gregoire@cegeka.com>
2017-07-15 11:01:37 +0200

    Created a method with the location assert inside of it, extracted player name to field I am feeling: Happy that the tests are more clean and readable Right now I have just finished: Created a method with the location assert inside of it, extracted player name to field Next up is: Refactoring the way the category is found I think I can improve in: More testing of other parts of the code to see if they break when I refactor

--- java/src/test/java/com/adaptionsoft/games/trivia/CategoryTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/CategoryTest.java
@@ -11,108 +11,85 @@ public class CategoryTest extends ConsoleOutputTest {
     private String POP = "Pop";
     private String SCIENCE = "Science";
     private String SPORTS = "Sports";
+    private String PLAYER_NAME = "Bart";
 
     @Test
     public void givenPlayer_whenInLocation0AfterRoll_thenCurrentCategoryIsPop() {
-        createGameWithPlayersAndRoll(0, "Bart");
-
-        assertLocationEquals(0);
+        createGameWithPositionAfterRoll(0);
         assertCategoryEquals(POP);
     }
 
     @Test
     public void givenPlayer_whenInLocation1AfterRoll_thenCurrentCategoryIsScience() {
-        createGameWithPlayersAndRoll(1, "Bart");
-
-        assertLocationEquals(1);
+        createGameWithPositionAfterRoll(1);
         assertCategoryEquals(SCIENCE);
     }
 
     @Test
     public void givenPlayer_whenInLocation2AfterRoll_thenCurrentCategoryIsSports() {
-        createGameWithPlayersAndRoll(2, "Bart");
-
-        assertLocationEquals(2);
+        createGameWithPositionAfterRoll(2);
         assertCategoryEquals(SPORTS);
     }
 
     @Test
     public void givenPlayer_whenInLocation3AfterRoll_thenCurrentCategoryIsRock() {
-        createGameWithPlayersAndRoll(3, "Bart");
-
-        assertLocationEquals(3);
+        createGameWithPositionAfterRoll(3);
         assertCategoryEquals(ROCK);
     }
 
     @Test
     public void givenPlayer_whenInLocation4AfterRoll_thenCurrentCategoryIsPop() {
-        createGameWithPlayersAndRoll(4, "Bart");
-
-        assertLocationEquals(4);
+        createGameWithPositionAfterRoll(4);
         assertCategoryEquals(POP);
     }
 
     @Test
     public void givenPlayer_whenInLocation5AfterRoll_thenCurrentCategoryIsScience() {
-        createGameWithPlayersAndRoll(5, "Bart");
-
-        assertLocationEquals(5);
+        createGameWithPositionAfterRoll(5);
         assertCategoryEquals(SCIENCE);
     }
 
     @Test
     public void givenPlayer_whenInLocation6AfterRoll_thenCurrentCategoryIsSports() {
-        createGameWithPlayersAndRoll(6, "Bart");
-
-        assertLocationEquals(6);
+        createGameWithPositionAfterRoll(6);
         assertCategoryEquals(SPORTS);
     }
 
     @Test
     public void givenPlayer_whenInLocation7AfterRoll_thenCurrentCategoryIsRock() {
-        createGameWithPlayersAndRoll(7, "Bart");
-
-        assertLocationEquals(7);
+        createGameWithPositionAfterRoll(7);
         assertCategoryEquals(ROCK);
     }
 
     @Test
     public void givenPlayer_whenInLocation8AfterRoll_thenCurrentCategoryIsPop() {
-        createGameWithPlayersAndRoll(8, "Bart");
-
-        assertLocationEquals(8);
+        createGameWithPositionAfterRoll(8);
         assertCategoryEquals(POP);
     }
 
     @Test
     public void givenPlayer_whenInLocation9AfterRoll_thenCurrentCategoryIsScience() {
-        createGameWithPlayersAndRoll(9, "Bart");
-
-        assertLocationEquals(9);
+        createGameWithPositionAfterRoll(9);
         assertCategoryEquals(SCIENCE);
     }
 
     @Test
     public void givenPlayer_whenInLocation10AfterRoll_thenCurrentCategoryIsSports() {
-        createGameWithPlayersAndRoll(10, "Bart");
-
-        assertLocationEquals(10);
+        createGameWithPositionAfterRoll(10);
         assertCategoryEquals(SPORTS);
     }
 
     @Test
     public void givenPlayer_whenInLocation11AfterRoll_thenCurrentCategoryIsRock() {
-        createGameWithPlayersAndRoll(11, "Bart");
-
-        assertLocationEquals(11);
+        createGameWithPositionAfterRoll(11);
         assertCategoryEquals(ROCK);
     }
 
     @Test
     public void givenPlayer_whenRoll12_thenLocationIs0() {
-        createGameWithPlayersAndRoll(12, "Bart");
+        createGameWithPlayersAndRoll(12, PLAYER_NAME);
 
-        String expectedOutputForLocationLine = "Bart's new location is 0";
+        String expectedOutputForLocationLine = PLAYER_NAME + "'s new location is 0";
 
         String[] outputLines = getOutputInLines();
         String actualOutputForLocationLine = outputLines[4];
@@ -121,7 +98,7 @@ public class CategoryTest extends ConsoleOutputTest {
     }
 
     private void assertLocationEquals(int location) {
-        String expectedOutputForLocationLine = "Bart's new location is " + location;
+        String expectedOutputForLocationLine = PLAYER_NAME + "'s new location is " + location;
         String actualOutputForLocationLine = getLocationLineFromConsole();
         assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
     }
@@ -140,6 +117,12 @@ public class CategoryTest extends ConsoleOutputTest {
     private String getCategoryLineFromConsole() {
         String[] outputLines = getOutputInLines();
         return outputLines[5];
+    }
+
+    private void createGameWithPositionAfterRoll(int roll) {
+        createGameWithPlayersAndRoll(roll, PLAYER_NAME);
+
+        assertLocationEquals(roll);
     }
 
     private void createGameWithPlayersAndRoll(int roll, String... playerNames) {
