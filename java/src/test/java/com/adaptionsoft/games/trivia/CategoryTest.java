adf792b4a0a0e12a7d02cc58772f9a2c0a11862c
Maarten Gregoire <maarten.gregoire@cegeka.com>
2017-07-15 10:50:58 +0200

    Extracting the assertions in new methods I am feeling: Happy that the tests are more readable Right now I have just finished: Extracting the assertions in new methods Next up is: Doing the locationassert in a different place, because it's not something we're actually testing, it's something we assert to be sure we're testing what we want to test I think I can improve in: making the tests cleaner

--- java/src/test/java/com/adaptionsoft/games/trivia/CategoryTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/CategoryTest.java
@@ -7,184 +7,105 @@ import static junit.framework.TestCase.assertEquals;
 
 public class CategoryTest extends ConsoleOutputTest {
 
+    private String ROCK = "Rock";
+    private String POP = "Pop";
+    private String SCIENCE = "Science";
+    private String SPORTS = "Sports";
+
     @Test
     public void givenPlayer_whenInLocation0AfterRoll_thenCurrentCategoryIsPop() {
         createGameWithPlayersAndRoll(0, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 0";
-        String expectedOutputForCategoryLine = "The category is Pop";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(0);
+        assertCategoryEquals(POP);
     }
 
     @Test
     public void givenPlayer_whenInLocation1AfterRoll_thenCurrentCategoryIsScience() {
         createGameWithPlayersAndRoll(1, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 1";
-        String expectedOutputForCategoryLine = "The category is Science";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(1);
+        assertCategoryEquals(SCIENCE);
     }
 
     @Test
     public void givenPlayer_whenInLocation2AfterRoll_thenCurrentCategoryIsSports() {
         createGameWithPlayersAndRoll(2, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 2";
-        String expectedOutputForCategoryLine = "The category is Sports";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(2);
+        assertCategoryEquals(SPORTS);
     }
+
     @Test
     public void givenPlayer_whenInLocation3AfterRoll_thenCurrentCategoryIsRock() {
         createGameWithPlayersAndRoll(3, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 3";
-        String expectedOutputForCategoryLine = "The category is Rock";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(3);
+        assertCategoryEquals(ROCK);
     }
 
     @Test
     public void givenPlayer_whenInLocation4AfterRoll_thenCurrentCategoryIsPop() {
         createGameWithPlayersAndRoll(4, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 4";
-        String expectedOutputForCategoryLine = "The category is Pop";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(4);
+        assertCategoryEquals(POP);
     }
 
     @Test
     public void givenPlayer_whenInLocation5AfterRoll_thenCurrentCategoryIsScience() {
         createGameWithPlayersAndRoll(5, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 5";
-        String expectedOutputForCategoryLine = "The category is Science";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(5);
+        assertCategoryEquals(SCIENCE);
     }
 
     @Test
     public void givenPlayer_whenInLocation6AfterRoll_thenCurrentCategoryIsSports() {
         createGameWithPlayersAndRoll(6, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 6";
-        String expectedOutputForCategoryLine = "The category is Sports";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(6);
+        assertCategoryEquals(SPORTS);
     }
 
     @Test
     public void givenPlayer_whenInLocation7AfterRoll_thenCurrentCategoryIsRock() {
         createGameWithPlayersAndRoll(7, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 7";
-        String expectedOutputForCategoryLine = "The category is Rock";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(7);
+        assertCategoryEquals(ROCK);
     }
 
     @Test
     public void givenPlayer_whenInLocation8AfterRoll_thenCurrentCategoryIsPop() {
         createGameWithPlayersAndRoll(8, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 8";
-        String expectedOutputForCategoryLine = "The category is Pop";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(8);
+        assertCategoryEquals(POP);
     }
 
     @Test
     public void givenPlayer_whenInLocation9AfterRoll_thenCurrentCategoryIsScience() {
         createGameWithPlayersAndRoll(9, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 9";
-        String expectedOutputForCategoryLine = "The category is Science";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(9);
+        assertCategoryEquals(SCIENCE);
     }
 
     @Test
     public void givenPlayer_whenInLocation10AfterRoll_thenCurrentCategoryIsSports() {
         createGameWithPlayersAndRoll(10, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 10";
-        String expectedOutputForCategoryLine = "The category is Sports";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(10);
+        assertCategoryEquals(SPORTS);
     }
 
     @Test
     public void givenPlayer_whenInLocation11AfterRoll_thenCurrentCategoryIsRock() {
-
         createGameWithPlayersAndRoll(11, "Bart");
 
-        String expectedOutputForLocationLine = "Bart's new location is 11";
-        String expectedOutputForCategoryLine = "The category is Rock";
-
-        String[] outputLines = getOutputInLines();
-        String actualOutputForLocationLine = outputLines[4];
-        String actualOutputForCategoryLine = outputLines[5];
-
-        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
-        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+        assertLocationEquals(11);
+        assertCategoryEquals(ROCK);
     }
 
     @Test
@@ -197,6 +118,28 @@ public class CategoryTest extends ConsoleOutputTest {
         String actualOutputForLocationLine = outputLines[4];
 
         assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
+    }
+
+    private void assertLocationEquals(int location) {
+        String expectedOutputForLocationLine = "Bart's new location is " + location;
+        String actualOutputForLocationLine = getLocationLineFromConsole();
+        assertEquals(expectedOutputForLocationLine, actualOutputForLocationLine);
+    }
+
+    private void assertCategoryEquals(String category) {
+        String expectedOutputForCategoryLine = "The category is " + category;
+        String actualOutputForCategoryLine = getCategoryLineFromConsole();
+        assertEquals(expectedOutputForCategoryLine, actualOutputForCategoryLine);
+    }
+
+    private String getLocationLineFromConsole() {
+        String[] outputLines = getOutputInLines();
+        return outputLines[4];
+    }
+
+    private String getCategoryLineFromConsole() {
+        String[] outputLines = getOutputInLines();
+        return outputLines[5];
     }
 
     private void createGameWithPlayersAndRoll(int roll, String... playerNames) {
