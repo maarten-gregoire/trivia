a058555e44dd32e3fb286fdc4cd4c1eb63b75f9b
Maarten Gregoire <maarten.gregoire@cegeka.com>
2017-07-16 22:27:11 +0200

    I added goldCoins to the player class and a method to add gold coins, I also wrote tests for this. I am feeling: eager to refactor this in the game Right now I have just finished:  adding goldCoins to the player class and a method to add gold coins, I also wrote tests for this. Next up is: using the goldCoins from the player object in the game code I think I can improve in:  All the things mentioned before :)

--- java/src/main/java/com/adaptionsoft/games/trivia/Player.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/Player.java
@@ -3,10 +3,12 @@ package com.adaptionsoft.games.trivia;
 public class Player {
     private String name;
     private int number;
+    private int goldCoins;
 
     public Player(String name, int number) {
         this.name = name;
         this.number = number;
+        this.goldCoins = 0;
     }
 
     public static Player aPlayer(String name, int number) {
@@ -19,5 +21,13 @@ public class Player {
 
     public int getNumber() {
         return number;
+    }
+
+    public int getGoldCoins() {
+        return goldCoins;
+    }
+
+    public int addGoldCoin() {
+        return ++goldCoins;
     }
 }
---
+++ java/src/test/java/com/adaptionsoft/games/trivia/PlayerTest.java
@@ -0,0 +1,29 @@
+package com.adaptionsoft.games.trivia;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class PlayerTest {
+
+    @Test
+    public void givenNewPlayer_thenGoldenCoinsIs0() {
+        Player player = new Player("Will", 1);
+
+        assertEquals(player.getGoldCoins(), 0);
+    }
+
+    @Test
+    public void givenNewPlayer_whenAddGoldCoin_thenGoldenCoinsIs1() {
+        Player player = new Player("Will", 1);
+
+        assertEquals(player.addGoldCoin(), 1);
+    }
+
+    @Test
+    public void givenNewPlayer_whenAdd2GoldCoins_thenGoldenCoinsIs2() {
+        Player player = new Player("Will", 1);
+        player.addGoldCoin();
+        assertEquals(player.addGoldCoin(), 2);
+    }
+}
